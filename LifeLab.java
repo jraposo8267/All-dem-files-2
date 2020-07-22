241bc1163b45c710c75f7c5e39d2a8e8354d8c31
Jorge Raposo <jorgealberto.p.raposo@hotmail.com>
2020-07-22 13:32:13 -0700

    Create LifeLab.java
    test

---
+++ LifeLab.java
@@ -0,0 +1,13 @@
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.StackPane.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.scene.text.Text;
+import javafx.scene.control.*;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+
+//this is a test, but might be my life lab lab idk lol
